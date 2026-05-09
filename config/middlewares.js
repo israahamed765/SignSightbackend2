42f5027fd036c14c1c00c3b9ab693156ffccb71e
israahamed765 <tsraathmd@gmail.com>
2026-05-09 11:32:26 +0300

    Initial commit - Added lesson page and API configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // يسمح بالوصول من أي رابط (مثالي لمرحلة التطوير والنشر الأولية)
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
